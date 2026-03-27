647e1b37b891316abefa31f2a174ae82798a0deb
Ales Verbic <verbotenj@blinklabs.io>
2026-03-27 16:14:36 -0400

    chore: upgrade adder to v0.39.1 (#95)
    Signed-off-by: Ales Verbic <verbotenj@blinklabs.io>

--- cmd/event-address-filter/main.go
+++ cmd/event-address-filter/main.go
@@ -19,7 +19,7 @@ import (
 	"log/slog"
 
 	"github.com/blinklabs-io/adder/event"
-	filter_chainsync "github.com/blinklabs-io/adder/filter/chainsync"
+	filter_chainsync "github.com/blinklabs-io/adder/filter/cardano"
 	filter_event "github.com/blinklabs-io/adder/filter/event"
 	input_chainsync "github.com/blinklabs-io/adder/input/chainsync"
 	output_embedded "github.com/blinklabs-io/adder/output/embedded"
